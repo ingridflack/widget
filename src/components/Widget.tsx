61896fb80da72140bbf16302f2e644c376c6427c
ingridflack <ingrid.flack@newtail.com.br>
2022-05-03 19:28:42 -0300

    config: Project configuration

---
+++ src/components/Widget.tsx
@@ -0,0 +1,22 @@
+import { ChatTeardropDots } from "phosphor-react";
+import { useState } from "react";
+
+import { Popover } from "@headlessui/react";
+
+const Widget = () => {
+  return (
+    <Popover className="absolute bottom-4 right-4">
+      <Popover.Panel>Hello world</Popover.Panel>
+
+      <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+        <ChatTeardropDots className="w-6 h-6" />
+
+        <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+          <span className="pl-2">Feedback</span>
+        </span>
+      </Popover.Button>
+    </Popover>
+  );
+};
+
+export default Widget;
